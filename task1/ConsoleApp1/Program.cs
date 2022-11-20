68330fddabb3970d190437f9b5633b7b7de30037
Mehrac Mammadov <mehrajvm@code.edu.az>
2022-11-20 17:38:44 +0400

    HomeWork Done

---
+++ task1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string str = "i HAve nOt FAilEd. i'Ve JuSt FOund 10,000 wAYs THat wON't WOrK.";
+
+static void correcting(string str)
+{
+
+    string tolower = str.ToLower();
+    string[] split = tolower.Split();
+    
+    foreach(string item in split)
+    {
+        //Console.WriteLine(item);
+        string remove = item.Remove(0,1);
+        //Console.WriteLine(remove);
+        string remove2 = item.Remove(1);
+        //Console.WriteLine(remove2);
+        string toupper = remove2.ToUpper();
+        //Console.WriteLine(toupper);
+        string replace = item.Replace(remove2, toupper);
+        Console.WriteLine(replace);
+
+    }
+}
+
+correcting(str);
---
+++ task2/HomeWork-18-11-22/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using HomeWork_18_11_22;
+#region ad ve soyad
+
+Student FullName = new Student("Ali", "Rizayev");
+
+#endregion
+
+
+#region butun deyerler
+
+//Student StudentResults = new Student("Ali", "Rizayev", "P230", 75, true);
+
+#endregion
+
+#region exam
+
+//Student StudentResults = new Student("Ali", "Rizayev", "P230", 75);
+
+#endregion
